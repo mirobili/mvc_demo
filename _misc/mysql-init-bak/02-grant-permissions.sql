97145f334dc92fda5bdd7989f4f8131f16b00941
mirobili <mirobili@mail.bg>
2024-10-17 15:24:56 +0300

    updates

--- _misc/mysql-init-bak/02-grant-permissions.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER IF NOT EXISTS 'mvc_demo'@'%' IDENTIFIED  WITH 'caching_sha2_password' BY 'mvc_demo2004';
-GRANT ALL PRIVILEGES ON mvc_demo.* TO 'mvc_demo'@'%';
-FLUSH PRIVILEGES;
-
-
-ALTER USER 'mvc_demo'@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'mvc_demo2004';
-FLUSH PRIVILEGES;
